3ff886663a14d217ec98a26f3d064d7088b0ddec
Abinash Pegu <44534811+abinash18123@users.noreply.github.com>
2023-12-27 17:42:28 +0530

    Add files via upload

--- day1.py
+++ day1.py
@@ -1,58 +1,20 @@
-def solve(arr):
-	global dic 
-	dic = {}
-	total = 0
+def summ(n):
+	flag = 0 
+	finalNum = ''
+	nums = ['one','two','three','four','five','six','seven','eight','nine']
+	dic = {'one':str(1),'two':str(2),'three':str(3),'four':str(4),'five':str(5),'six':str(6),'seven':str(7),'eight':str(8),'nine':str(9)}
+
+	timline = {}
+	for k in nums:
+		for num in range(0,len(n) - len(k) + 1):
+			number = n[num:num+len(k)]
+			if number == k:
+				timline[num] = dic[k]
+
+	for j in range(len(n)):
+		if n[j].isdigit(): 
+			timline[j] = n[j]
+	indexes = sorted(timline)
+	return int(timline[indexes[0]] + timline[indexes[-1]])
 	
-	for i in range(len(arr)):
-		num = ''
-		indices = []
-		for j in range(len(arr[i])):
-			curr = arr[i][j]
-			if curr.isdigit():
-				num += curr 
-				indices.append([i,j])
-			else:
-				if len(num) != 0:
-					findNum(num,indices,arr)
-					num = ''
-					indices = []
-		if len(num) !=0:
-			findNum(num,indices,arr)
-
-	for i in dic:
-		if len(dic[i]) == 2:
-			total += int(dic[i][0])*int(dic[i][1])
-	return total
-	
-def findNum(num,indices,arr):
-	mapper = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-	for i in indices:
-		x = i[0]
-		y = i[1]
-		for j in mapper:
-			newx = x + j[0]
-			newy = y + j[1]
-			booll = validCheck(arr,newx,newy)
-			if booll:
-				coor = (newx,newy)
-				if coor in dic: 
-					dic[coor].append(num)
-				else: 
-					dic[coor]=[num]
-				break
-		if booll: 
-			break
-			
-			
-def validCheck(arr,newx,newy):
-	if 0 <= newx <= len(arr)-1 and 0 <= newy <= len(arr[0]) -1:
-		newnum =  arr[newx][newy]
-		if not newnum.isdigit() and newnum == '*':
-			return True
-	return False
-				
-print(solve([(i.rstrip()) for i in open("input.txt", "r")]))
-
-
-
-
+print(sum([summ(i.rstrip()) for i in open("input.txt", "r")]))
